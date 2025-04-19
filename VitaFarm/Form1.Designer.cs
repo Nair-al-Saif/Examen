336e928a4d1c88f6b60e4b06e2a071c7ed3ce218
Александр Хлюстов <144451476+Nair-al-Saif@users.noreply.github.com>
2025-04-19 10:00:52 +0500

    20250419-1003

--- VitaFarm/Form1.Designer.cs
+++ VitaFarm/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VitaFarm
 {
-    partial class Form1
+    partial class FormLogin
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(520, 323);
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
